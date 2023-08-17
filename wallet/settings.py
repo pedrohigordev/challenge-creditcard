9ca35f54c0d357db922110b81a5e990d48bdef39
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-08-17 18:54:03 -0300

    refactor:Correcting DRF configuration

--- wallet/settings.py
+++ wallet/settings.py
@@ -131,6 +131,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permission.IsAuthenticatedOrReadOnly'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     )
 }
