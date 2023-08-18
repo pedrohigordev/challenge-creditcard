dff01dffa144da7da8e33c2377aebc08ced85c6d
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-08-17 21:45:55 -0300

    feat: Create card API

--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Card
 from .serializers import CardSerializer
@@ -10,3 +11,9 @@ class CardApiView(APIView):
         cards = Card.objects.all()
         serializer = CardSerializer(cards, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CardSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"msg": "Created"}, status=status.HTTP_201_CREATED)
