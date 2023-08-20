8834b6714907fcce0f78cf173bb9d63c4b75c7c8
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-08-20 02:08:22 -0300

    refact: Implementing mixins in viewsets

--- cards/views.py
+++ cards/views.py
@@ -1,10 +1,16 @@
-from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework import mixins
 
 from .models import Card
 from .serializers import CardSerializer
 
 
-class CardViewSet(viewsets.ModelViewSet):
+class CardViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
