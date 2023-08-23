e498bd1512866f614670011b5fab5a28a084b436
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-08-23 13:09:45 -0300

    fix: Connecting database postgres

--- wallet/settings.py
+++ wallet/settings.py
@@ -59,10 +59,15 @@ WSGI_APPLICATION = 'wallet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maistodos',
+        'USER': 'postgres',
+        'PASSWORD': 'maistodos',
+        'HOST': '172.21.0.2',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
