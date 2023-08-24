92c00641269790a3c2770f0c6ff801be2dfef59b
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-08-24 00:02:57 -0300

    fix: Correcting permission

--- wallet/settings.py
+++ wallet/settings.py
@@ -107,8 +107,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ),
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 
