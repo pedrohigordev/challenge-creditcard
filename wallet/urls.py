4af3ef0782f199440089ea714704eae491b5afc0
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-08-24 00:33:28 -0300

    feat: Adding swagger for documentation

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from wallet.apps.cards.urls import router
 
 from wallet.apps.cards.views import TokenJWTView
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Challenge Backend Python Creditcard",
+        default_version='v1',
+        description="API simples de cadastro de cartões de crédito.",
+        contact=openapi.Contact(email="pedrohigor.dev@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/token/', TokenJWTView.as_view(), name='token_jwt'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui')
 ]
